8745494238f7f90616472e128b7feb5c98ba513f
GCRocks <141965820+GCRocks@users.noreply.github.com>
2024-02-13 23:16:15 -0700

    finished. graph of breadboard is microsoft teams pic 49

--- src/hdl/thirtyOneDayMonth_tb.vhd
+++ src/hdl/thirtyOneDayMonth_tb.vhd
@@ -68,6 +68,7 @@ architecture test_bench of thirtyOneDayMonth_tb is
 
   -- declare any additional components required
   
+  signal w_sel: std_logic_vector (2 downto 0);
   signal w_sw : std_logic_vector (3 downto 0):= (others=> '0');
   signal w_Y : std_logic := '0';
 
@@ -93,7 +94,36 @@ begin
 		w_sw <= x"0"; wait for 10 ns;
             assert w_Y = '0' report "error on x0" severity failure;
         w_sw <= x"1"; wait for 10 ns;
-            assert w_Y = '1' report "error on Jan" severity failure;   
+            assert w_Y = '1' report "error on Jan" severity failure; 
+        w_sw <= x"2"; wait for 10 ns;
+            assert w_Y = '0' report "error on Feb" severity failure;
+        w_sw <= x"3"; wait for 10 ns;
+            assert w_Y = '1' report "error on March" severity failure; 
+        w_sw <= x"4"; wait for 10 ns;
+            assert w_Y = '0' report "error on April" severity failure;
+        w_sw <= x"5"; wait for 10 ns;
+            assert w_Y = '1' report "error on May" severity failure; 
+        w_sw <= x"6"; wait for 10 ns;
+            assert w_Y = '0' report "error on June" severity failure;
+        w_sw <= x"7"; wait for 10 ns;
+            assert w_Y = '1' report "error on July" severity failure;  
+        w_sw <= x"8"; wait for 10 ns;
+            assert w_Y = '1' report "error on August" severity failure; 
+        w_sw <= x"9"; wait for 10 ns;
+            assert w_Y = '0' report "error on September" severity failure;
+        w_sw <= x"10"; wait for 10 ns;
+            assert w_Y = '1' report "error on October" severity failure; 
+        w_sw <= x"11"; wait for 10 ns;
+            assert w_Y = '0' report "error on November" severity failure;
+        w_sw <= x"12"; wait for 10 ns;
+            assert w_Y = '1' report "error on December" severity failure;
+        w_sw <= x"13"; wait for 10 ns;
+            assert w_Y = '0' report "error on x13" severity failure;
+        w_sw <= x"14"; wait for 10 ns;
+            assert w_Y = '0' report "error on x14" severity failure;
+        w_sw <= x"15"; wait for 10 ns;
+            assert w_Y = '0' report "error on x15" severity failure;
+                      
 
 		wait; -- wait forever
 	end process;	
